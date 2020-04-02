91dbd17c6bac03528e174ea46ad0185c04a1ace7
KaannestadPanda <37317621+KaannestadPanda@users.noreply.github.com>
2020-04-01 20:00:44 -0700

    dynamic int array

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -43,6 +43,7 @@ private int[] intArray;
 	
 	//6. Complete the steps in the set method
 	public void set(int v, int location) {
+		intArray[location]=v;
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
 	}
@@ -51,9 +52,20 @@ private int[] intArray;
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
+		int[] intArray2=new int[intArray.length+1];
 		
 		//B. Make a for loop that iterates through the new array
-
+for(int i = 0; i < intArray2.length; i++) {
+	if(i<location) {
+		intArray2[i]=intArray[i];
+	}
+	else if(i>location) {
+		intArray2[i]=intArray[i-1];
+	}
+	else {
+		intArray2[i]=v;
+	}
+}
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
 			
@@ -62,7 +74,7 @@ private int[] intArray;
 			
 			//E. else, set the element at i of the new array to the value v
 		
-		
+		intArray = intArray2;
 		//F. set the member array equal to the new array
 	
 	}
@@ -72,30 +84,38 @@ private int[] intArray;
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int[] intArray3=new int[intArray.length-2];
 		//B. make a for loop to iterate through the member array
-		
+		for(int i = 0; i <intArray.length;i++) {
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
-			
+			if(i<location) {
+				intArray3[i]=intArray[i];
+			}
+			else if(i>location) {
+				intArray3[i-1]=intArray[i];
+			}
 			//D. else if i  is greater than location
 			//		set the element at i - 1 of the new array to the element at i of the member array
 			
 			//E. else, continue;
 			
+		}
 		//F. set the member array equal to the new array
+		intArray=intArray3;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		return intArray.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		intArray=new int[0];
 	}
 	
 	//13. Run the test again to see if you are finished.
