16ae7d44a583e72363ed00174594cbc03efb5415
KaannestadPanda <37317621+KaannestadPanda@users.noreply.github.com>
2020-04-08 19:58:40 -0700

    guestBook cuz i forgot to save this part

--- src/_01_IntroToArrayLists/_02_GuestBook.java
+++ src/_01_IntroToArrayLists/_02_GuestBook.java
@@ -2,12 +2,19 @@ package _01_IntroToArrayLists;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class _02_GuestBook {
+public class _02_GuestBook implements ActionListener {
+	ArrayList<String> nameList = new ArrayList<String>();
+	JButton addButton = new JButton();
+	JButton viewButton = new JButton();
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -18,10 +25,20 @@ public class _02_GuestBook {
 	// Guest #4: Donny Doners
 	
 	public static void main(String[] args) {
+		_02_GuestBook a = new _02_GuestBook();
+		a.setup();
+		
+	
+	}
+	
+	
+	void setup() {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		JButton addButton = new JButton();
-		JButton viewButton = new JButton();
+		
+		addButton.addActionListener(this);
+		viewButton.addActionListener(this);
+		
 		Dimension dimension = new Dimension(400,400);
 		addButton.setPreferredSize(dimension);
 		viewButton.setPreferredSize(dimension);
@@ -36,7 +53,36 @@ public class _02_GuestBook {
 		panel.add(viewButton);
 		frame.pack();
 		frame.setVisible(true);
-	
+		
+		
 	}
+	
+	void addName() {
+		String namae= JOptionPane.showInputDialog("ENTER A NAME YOU FREAK");
+		nameList.add(namae);
+	}
+	
+	void viewNames() {
+		String summary = "";
+		
+		JOptionPane.showMessageDialog(null, summary);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==addButton) {
+			addName();
+		}
+		else if(e.getSource()==viewButton) {
+			viewNames();
+		}
+	}
+	
+	
+	
+	
+	
 	
 }
