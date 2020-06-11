70a7e3cd08e01e60184afcc57264d117041f47c8
KaannestadPanda <37317621+KaannestadPanda@users.noreply.github.com>
2020-06-10 20:01:24 -0700

    stacks

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,15 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 
@@ -13,7 +22,77 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	Stack<Character> stak =new Stack<Character>();
 	
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();	
+JLabel label = new JLabel();
+
+
+
+	public static void main(String[] args) {
+		
+		_02_TextUndoRedo a = new _02_TextUndoRedo();
+		a.setup();
+	
+
+	}
+	
+	void setup() {
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		Dimension d = new Dimension(400,400);
+		label.setPreferredSize(d);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+		
+		if(e.getKeyCode()==8) {
+			String g = label.getText();
+			if(g.length()>0) {
+				Character savedDeletion=g.charAt(g.length()-1);
+				stak.push(savedDeletion);
+				
+				
+			String newText = g.substring(0, g.length()-1);
+			label.setText(newText);
+			}
+		}
+		else if(e.getKeyCode()==17) {
+			if(stak.size()>0) {
+			label.setText(label.getText()+stak.pop());
+			}
+		}
+		
+	else {
+		String g = label.getText();
+		e.getKeyChar();
+		label.setText(g+e.getKeyChar());
+	}
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 	
 }
--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -19,9 +19,42 @@ public class _03_TestMatchingBrackets {
 	}
 
 	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
+	
+//	public static void main(String[] args) {
+//		_03_TestMatchingBrackets a = new _03_TestMatchingBrackets();
+//		a.setup();
+//	}
+//	
+//	void setup() {
+//		
+//	}
+	
+	
 	private boolean doBracketsMatch(String b) {
+		Stack<Character> stack = new Stack<Character>();
+		for(int i = 0; i<b.length();i++) {
+			stack.push(b.charAt(i));
+			b=b.substring(i+1);
+		}
 		
+		int counter = 0;
+		for(int k = 0; k<stack.size();k++) {
+			if(stack.pop()=='{') {
+				counter++;
+			}
+			else if(stack.pop()=='}') {
+				counter--;
+			}
+			
+		}
+		
+		if(counter==0) {
+			return true;
+		}
+		else {
 		return false;
+		
+		}
 	}
 
 }
