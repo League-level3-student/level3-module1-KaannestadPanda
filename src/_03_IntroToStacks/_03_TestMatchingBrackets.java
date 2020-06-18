9f57d3b590082e6d8de18d5b3767c27832c11778
KaannestadPanda <37317621+KaannestadPanda@users.noreply.github.com>
2020-06-17 20:02:14 -0700

    more stacks

--- src/_03_IntroToStacks/_03_TestMatchingBrackets.java
+++ src/_03_IntroToStacks/_03_TestMatchingBrackets.java
@@ -18,8 +18,8 @@ public class _03_TestMatchingBrackets {
 		assertFalse(doBracketsMatch("}{"));
 	}
 
-	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS A MATCHING CLOSING BRACKET
-	
+	// USE A STACK TO COMPLETE THE METHOD FOR CHECKING IF EVERY OPENING BRACKET HAS
+	// A MATCHING CLOSING BRACKET
 //	public static void main(String[] args) {
 //		_03_TestMatchingBrackets a = new _03_TestMatchingBrackets();
 //		a.setup();
@@ -28,33 +28,57 @@ public class _03_TestMatchingBrackets {
 //	void setup() {
 //		
 //	}
-	
-	
 	private boolean doBracketsMatch(String b) {
 		Stack<Character> stack = new Stack<Character>();
-		for(int i = 0; i<b.length();i++) {
-			stack.push(b.charAt(i));
-			b=b.substring(i+1);
-		}
 		
-		int counter = 0;
-		for(int k = 0; k<stack.size();k++) {
-			if(stack.pop()=='{') {
-				counter++;
+		for (int i = 0; i < b.length(); i++) {
+			if(b.charAt(i)=='{') {
+				stack.push('g');
 			}
-			else if(stack.pop()=='}') {
-				counter--;
+			else if(b.charAt(i)=='}') {
+				if(stack.size()>0) {
+				stack.pop();
+				}
+				else {
+					return false;
+				}
 			}
 			
+			
+			//stack.push(b.charAt(i));
+
 		}
-		
-		if(counter==0) {
-			return true;
+		if(stack.size()==0) {
+		return true;
 		}
 		else {
-		return false;
+			return false;
+			}
 		
-		}
+		
+
+//		int counter = 0;
+//		for (int k = 0; k < stack.size(); k++) {
+//			char popped = stack.pop();
+//			if (popped == '{') {
+//				counter++;
+//				System.out.println("leftbrak");
+//			} else if (popped == '}') {
+//				counter--;
+//				System.out.println("rightbrak");
+//			}
+//			if (counter > 0) {
+//				return false;
+//			}
+//
+//		}
+//
+//		if (counter == 0) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+
 	}
 
 }
--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,67 @@
 package _04_HangMan;
 
-public class HangMan{
-	
-	
-	
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements KeyListener {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<String> strings = new Stack<String>();
+
+	public static void main(String[] args) {
+		HangMan a = new HangMan();
+		a.setup();
+	}
+
+	void setup() {
+		String inputNumber = JOptionPane
+				.showInputDialog("Give a number for the number of words and that's the number you'll give and get");
+		int inputInt = Integer.parseInt(inputNumber);
+		
+		for(int i = 0; i<inputInt;i++) {
+			String a = Utilities.readRandomLineFromFile("dictionary.txt");
+			strings.push(a);
+		}
+		
+
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		
+
+		Dimension d = new Dimension(400, 400);
+		label.setPreferredSize(d);
+
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
